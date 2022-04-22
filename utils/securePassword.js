c0f5c04f5ceca7219301596794a033748b81231e
Shoga <57602445+Olusoga@users.noreply.github.com>
2022-04-22 08:20:31 +0100

    initial commit

--- utils/securePassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-const securePassword = async (password) =>{
-    const salt = await genSalt(10);
-    const hashedpassword = await bycrypt.hash(password, salt)
-    return hashedpassword;
-};
-
-module.exports = securePassword;
